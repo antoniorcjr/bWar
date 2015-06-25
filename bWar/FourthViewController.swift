24c0d328437cde33ffdfcda55e6af83a1357e16e
Visitor <adrianaizel@gmail.com>
2015-06-25 17:21:43 -0400

    Incluindo Tab Third e Fourth

---
+++ bWar/FourthViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FourthViewController.swift
+//  bWar
+//
+//  Created by Adriana Izel on 6/25/15.
+//  Copyright © 2015 BEPiD. All rights reserved.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
