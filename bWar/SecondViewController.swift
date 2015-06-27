151facada391bb115445d54a6febd118681c6f9b
Antonio Jr <antonio.rcjr@gmail.com>
2015-06-27 10:56:32 -0400

    Componentes da tela de mímica.

--- bWar/SecondViewController.swift
+++ bWar/SecondViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    @IBOutlet weak var teamLabel: UILabel!
+    @IBAction func indexChanged(sender: AnyObject) {
+        switch segmentControl.selectedSegmentIndex
+        {
+        case 0:
+            teamLabel.text = "Team 1";
+        case 1:
+            teamLabel.text = "Team 2";
+        default:
+            break; 
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +34,5 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
