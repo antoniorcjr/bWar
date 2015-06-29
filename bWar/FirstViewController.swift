e87c426ebc4a4d8e7a8e6b6948b55932fd4b25ea
Adriana Izel <adrianaizel@gmail.com>
2015-06-29 18:36:16 -0400

    Tela de Rank - Outlets
    Ligando View com Controller (outlets).
    * Tive que excluir o bgVermelho.png do projeto, pois estava gerando erro

--- bWar/FirstViewController.swift
+++ bWar/FirstViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var lbNameT01: UILabel!
+    @IBOutlet weak var lbScoreT01: UILabel!
+    @IBOutlet weak var lbPointsT01: UILabel!
+    @IBOutlet weak var lbNameT02: UILabel!
+    @IBOutlet weak var lbScoreT02: UILabel!
+    @IBOutlet weak var lbPointsT02: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        lbNameT01.text = "Time A"
+        lbScoreT01.text = "0"
+        lbPointsT01.text = "10"
+        
+        lbNameT02.text = "Time B"
+        lbScoreT02.text = "0"
+        lbPointsT02.text = "10"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,29 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func incrementPointsTeam01(sender: AnyObject) {
+        let value: Int? = Int(lbPointsT01.text!)
+        lbPointsT01.text = "\(value! + 10)"
+    }
 
+    @IBAction func decrementPointsTeam01(sender: AnyObject) {
+        let value: Int? = Int(lbPointsT01.text!)
+        if (value != 0) {
+            lbPointsT01.text = "\(value! - 10)"
+        }
+    }
+
+    @IBAction func incrementPointsTeam02(sender: AnyObject) {
+        let value: Int? = Int(lbPointsT02.text!)
+        lbPointsT02.text = "\(value! + 10)"
+    }
+    
+    @IBAction func decrementPointsTeam02(sender: AnyObject) {
+        let value: Int? = Int(lbPointsT02.text!)
+        
+        if (value != 0) {
+            lbPointsT02.text = "\(value! - 10)"
+        }
+    }
 }
 
