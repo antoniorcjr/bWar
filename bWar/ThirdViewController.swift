21c3fbad17bc97777be1d5b8b715bb676f7c49d7
Antonio Jr <antonio.rcjr@gmail.com>
2015-07-02 00:48:18 -0400

    Finalização de funcionalidades da tela de prendas.

--- bWar/ThirdViewController.swift
+++ bWar/ThirdViewController.swift
@@ -13,12 +13,27 @@ class ThirdViewController: UIViewController {
     @IBOutlet weak var segmentControl: UISegmentedControl!
     
     @IBOutlet weak var lbJoking: UILabel!
+    @IBOutlet weak var background: UIImageView!
     
     @IBOutlet weak var lbNameT01: UILabel!
     @IBOutlet weak var lbPointsT01: UILabel!
     
     @IBOutlet weak var lbNameT02: UILabel!
     @IBOutlet weak var lbPointsT02: UILabel!
+    
+    @IBOutlet weak var noButton: UIButton!
+    @IBOutlet weak var okButton: UIButton!
+    
+    @IBAction func indexChanged(sender: AnyObject) {
+        switch segmentControl.selectedSegmentIndex {
+        case 0:
+            background.image = UIImage(named: "bgAzul")
+        case 1:
+            background.image = UIImage(named: "bgVermelho")
+        default:
+            break;
+        }
+    }
     
     lazy var daoTeam: BWDaoTeam = {
         return (UIApplication.sharedApplication().delegate as! AppDelegate).daoTeam
@@ -45,6 +60,9 @@ class ThirdViewController: UIViewController {
     }
     
     @IBAction func raffleJoking(sender: AnyObject) {
+        okButton.enabled = true
+        noButton.enabled = true
+        segmentControl.enabled = true
         print("raffleJoking")
         let imitations = daoTeam.getJokings()
         let randomNumber = Int(arc4random_uniform(10))
@@ -55,6 +73,10 @@ class ThirdViewController: UIViewController {
     }
     
     @IBAction func incrementPoints(sender: AnyObject) {
+        okButton.enabled = false
+        noButton.enabled = false
+        segmentControl.enabled = true
+        lbJoking.text = "?"
         let isTeamA = (segmentControl.selectedSegmentIndex == 0)
         UtilViewController().incrementPoints((isTeamA ? lbPointsT01 : lbPointsT02), isTeamA: isTeamA)
     }
@@ -62,6 +84,10 @@ class ThirdViewController: UIViewController {
     // TODO: codigo comentado pois nao acontece nada se errar,
     // o codigo anterior retirava ponto
     @IBAction func decrementPoints(sender: AnyObject) {
+        okButton.enabled = false
+        noButton.enabled = false
+        segmentControl.enabled = true
+        lbJoking.text = "?"
         //        let isTeamA = (segmentControl.selectedSegmentIndex == 0)
         //        UtilViewController().decrementPoints((isTeamA ? lbPointsT01 : lbPointsT02), isTeamA: isTeamA)
     }
